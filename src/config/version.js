e9885dba44338599cb34e978f31ec7d491573f81
lambert-mekong <mekongtandem@gmail.com>
2025-10-19 18:30:47 +0200

    Update version.js

--- src/config/version.js
+++ src/config/version.js
@@ -4,4 +4,4 @@
 export const APP_VERSION = "2.5.0";
 export const APP_NAME = "Mémoire du Mékong";
 export const BUILD_DATE = "19 octobre 2025";
-export const PHASE = "Phase 17 - Chat ↔️ Memories";
+export const PHASE = "Phase 17c - Chat ↔️ Memories";
